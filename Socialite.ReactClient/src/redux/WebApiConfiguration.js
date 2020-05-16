ba84a5e1d4a0a1b67c3806211c9deb7bd3bf4c29
Matt Ryder <matt@mattryder.co.uk>
2020-05-16 22:19:54 +0100

    Change that expected api host key

--- Socialite.ReactClient/src/redux/WebApiConfiguration.js
+++ Socialite.ReactClient/src/redux/WebApiConfiguration.js
@@ -3,7 +3,7 @@ import RuntimeConfigurationLoader from '../RuntimeConfigurationLoader';
 export default class WebApiConfiguration extends RuntimeConfigurationLoader {
     static keys() {
         return {
-            api_host: "REACT_APP_RUNTIME_WEB_API_HOST",
+            api_host: "REACT_APP_RUNTIME_API_HOST",
         };
     }
 }
